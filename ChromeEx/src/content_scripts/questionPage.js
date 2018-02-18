19c3080ceae9eef2a663cae464cd3da30c6f493b
Tammy Vu <tammylvu@berkeley.edu>
2018-02-17 17:38:33 -0700

    some of modal is working

--- ChromeEx/src/content_scripts/questionPage.js
+++ ChromeEx/src/content_scripts/questionPage.js
@@ -1,6 +1,10 @@
 var $watchIcon = null,
-  $notificationDiv = $('<div></div>').attr({id: 'se_notifier', title: 'Click to confirm'}),
-  $popup = $('<span><!-- The Modal --><div id="myModal" class="modal"><!-- Modal content --><div class="modal-content"><span class="close">&times;</span><p>Some text in the Modal..</p></div></div></span>');
+  $popup = $('<div><!-- The Modal --><div id="myModal" class="modal">\
+    <!-- Modal content --><div class="modal-content">\
+    <span class="close">&times;</span>\
+    <p><input id="bountVal" placeholder="Bounty Amount"/><input id="deadlineVal" placeholder="Deadline"/>\
+    <input id="Address" placeholder="Address"/>\
+    Some text in modal</p></div></div></div>');
 
 function sendMessageToBackground(message, callback) {
   chrome.runtime.sendMessage(message, callback);
@@ -9,7 +13,6 @@ function sendMessageToBackground(message, callback) {
 function notifyBackgroundForPageLoad() {
   var url = window.location.href,
     message = { event: 'pageLoaded', url: url, pageType: 'questionPage' };
-
   sendMessageToBackground(message, function() {});
 }
 
@@ -20,45 +23,26 @@ function createWatchIcon() {
     imageUrl = chrome.extension.getURL('resources/icons/eye-closed/128.png');
 
     // Get the <span> element that closes the modal
-    var span = $("close")[0];
+    // var span = $("close")[0];
 
   $watchIcon = $('<img>').attr({ id: 'watchIcon', src: imageUrl, title: 'set bounty' })
     .click(function() {
-      // $(document.body).append($modal);
-      $tester = $('<div></div>').attr({id: 'test'}),
-      $(document.body).addClass("hello");
-      console.log("dafjfa");
-      //$(popup).css("display", "block");
-
-      //modal.style.display = "block";
-      console.log("jk;j");
+      $(document.body).append($popup);
 
       var action = $(this).attr('data-action');
       // Update the watch button state ASAP. In case watch/un-watch fails,
       // the same is handled when message is received from background script.
       updateWatchIcon(action == 'watchPage');
 
-      if (action == 'watchPage') {
-        notificationText = 'CLICK HERE to confirm';
-      } else {
-        notificationText = 'Will do nothing eventually';
-      }
-
       showNotification({type: 'se_notice', message: notificationText});
 
       sendMessageToBackground({ action: action, url: url }, function(){ } );
    });
 
-  
-  // $notificationDiv.click(function() {
-  //   $(this).hide();
-  //   //put function here from .sol
-  // });
 
   $target = $('#question').find('div.vote').first();
   $target.append($watchIcon);
-  $(document.body).append($notificationDiv);
-  //$(document.body).append($popup);
+
 }
 
 function updateWatchIcon(watchStatus) {
@@ -68,9 +52,6 @@ function updateWatchIcon(watchStatus) {
   if (!$watchIcon) {
     createWatchIcon();
   } else {
-    // setTimeout(function() {
-    //   $notificationDiv.fadeOut(5000);
-    // }, 3000);
   }
 
   if (watchStatus) {
@@ -85,28 +66,9 @@ function updateWatchIcon(watchStatus) {
 }
 
 function showNotification(notification) {
-  var $bountyVal= $('<input/>').attr({ type: 'form', name:'bountval', placeholder:'Bounty'});
-  var $deadlineVal= $('<input/>').attr({ type: 'form', name:'deadlval', placeholder:'Deadline (in blocks)'});
-  var $ownAddrVal= $('<input/>').attr({ type: 'form', name:'ownaddrval', placeholder:'Address'});
-  var $button= $('<input/>').attr({ type: 'button', name:'confirm', value:'Confirm'});
   
-
-  $notificationDiv.text(notification.message)
-    .removeClass('se_notice se_error se_success').addClass(notification.type)
-    .fadeIn(1000);
-  $notificationDiv.append($bountyVal);
-  $notificationDiv.append($deadlineVal);
-  $notificationDiv.append($ownAddrVal);
-  $notificationDiv.append($button);
-
-  $button.click(function() {
-    $notificationDiv.remove();
-    //put function here from .sol
-  });
-
   // span.click(function() {
   //      modal.css("display", "none");
-
   // });
 
   // window.onclick = function(event) {
